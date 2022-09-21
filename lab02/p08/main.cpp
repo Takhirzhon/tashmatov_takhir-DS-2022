dc082fa5fa1531cb994f0188822c30813011f986
Takhirzhon <tashmatov_t@auca.kg>
2022-09-21 12:32:08 +0200

    Finish Problem 08  uVa ID11614

---
+++ lab02/p08/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    int counter = 0;
+
+    for (char ch; cin.get(ch);)
+    {
+        if (ch == '\"')
+        {
+            counter++;
+
+            if (counter % 2 != 0)
+            {
+                cout.put('`');
+                cout.put('`');
+            }
+            else
+            {
+                cout.put('\'');
+                cout.put('\'');
+            }
+        }
+        else
+        {
+            cout.put(ch);
+        }
+    }
+}
