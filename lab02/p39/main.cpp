12f390bcf8030946b89d93a7f3275346aa7233e6
Takhirzhon <tashmatov_t@auca.kg>
2022-10-11 15:04:46 +0200

    Finish problem 39 from 02

---
+++ lab02/p39/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    for (string line; getline(cin, line);)
+    {
+
+        bool inWord = false;
+        string word;
+        for (int i = 0; i < sz(line); i++)
+        {
+            if (!isspace(line[i]) && !inWord)
+            {
+                inWord = true;
+                word += line[i];
+            }
+            else if (!isspace(line[i]))
+            {
+                word += line[i];
+            }
+            else if (isspace(line[i]) && !word.empty())
+            {
+                reverse(word.begin(), word.end());
+                cout << word << line[i];
+                word.clear();
+                inWord = false;
+            }
+            else
+            {
+                cout << line[i];
+            }
+        }
+        if (!word.empty())
+        {
+            reverse(word.begin(), word.end());
+            cout << word;
+        }
+        cout << "\n";
+    }
+}
