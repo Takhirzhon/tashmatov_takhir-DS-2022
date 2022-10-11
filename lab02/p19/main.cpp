fa54c0a993bec4afafbe4de98130d260b776dff0
Takhirzhon <tashmatov_t@auca.kg>
2022-10-11 15:43:42 +0200

    Finish problem 19 from lab 2

--- lab02/p19/main.cpp
+++ lab02/p19/main.cpp
@@ -8,4 +8,30 @@ using namespace std;
 int main()
 {
     iostream::sync_with_stdio(false);
+
+    string str;
+    int test;
+    cin >> test;
+    cin.ignore();
+    while (test--)
+    {
+        getline(cin, str);
+        int i = 0;
+        int contain1 = 0;
+        int contain2 = 0;
+
+        while (str[i] != '\0')
+        {
+            if (str[i] == 'F')
+                contain1++;
+            else if (str[i] == 'M')
+                contain2++;
+            i++;
+        }
+        if (contain2 == contain1 && contain1 + contain2 >= 4)
+            cout << "LOOP" << endl;
+        else
+            cout << "NO LOOP" << endl;
+    }
+    return 0;
 }
