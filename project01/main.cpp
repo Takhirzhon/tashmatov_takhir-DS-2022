8293c2f95f955494cf45a5b72d6fb071da052c0a
Takhirzhon <tashmatov_t@auca.kg>
2022-11-14 07:58:26 +0100

    Started Project 1

---
+++ project01/main.cpp
@@ -0,0 +1,48 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../doctest/doctest.h"
+
+#include <sstream>
+#include <sstream>
+#include <string>
+#include "BigInt.hpp"
+
+using namespace std;
+
+TEST_CASE("Default constructor")
+{
+    BigInt x;
+    ostringstream sout;
+    sout << x;
+    REQUIRE(sout.str() == "0");
+
+    vector<BigInt> v(5);
+    for (const auto &e : v)
+    {
+        ostringstream sout2;
+        sout2 << e;
+        REQUIRE(sout2.str() == "0");
+    }
+}
+
+TEST_CASE("Constructor with  a string")
+{
+    ostringstream sout;
+    SUBCASE("postive number")
+    {
+        BigInt x("123456789123456789");
+        sout << x;
+        REQUIRE(sout.str() == "123456789123456789");
+    }
+
+    SUBCASE("negative number")
+    {
+        BigInt x("-123");
+        sout << x;
+        REQUIRE(sout.str() == "-123");
+    }
+
+    SUBCASE("empty string")
+    {
+        REQUIRE_THROWS_AS(BigInt(""), runtime_error);
+    }
+}
