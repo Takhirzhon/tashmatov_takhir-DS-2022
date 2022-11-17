97c588a2517353a770873a66c26ddc149f96e512
Takhirzhon <tashmatov_t@auca.kg>
2022-11-17 14:37:32 +0100

    Started Postove + negative

--- project01/BigInt.hpp
+++ project01/BigInt.hpp
@@ -7,6 +7,7 @@
 class BigInt
 {
     friend std::ostream &operator<<(std::ostream &out, const BigInt &x);
+    friend BigInt operator+(const BigInt &x, const BigInt &y);
 
     std::vector<int> mDigits;
     bool mIsNegative;
@@ -26,6 +27,44 @@ public:
             mDigits.push_back(d - '0');
         }
     }
+
+    static BigInt addAbsVaalues(const BigInt &x, const BigInt &y)
+    {
+        auto itX = x.mDigits.rbegin();
+        auto itY = y.mDigits.rbegin();
+
+        BigInt z;
+        z.mDigits.resize(std::max(x.mDigits.size(), y.mDigits.size()) + 1);
+        auto itZ = z.mDigits.rbegin();
+
+        int carry = 0;
+        while (itX != x.mDigits.rend() || itY != y.mDigits.rend())
+        {
+            int s = carry;
+            if (itX != x.mDigits.rend())
+            {
+                s += *itX;
+                ++itX;
+            }
+            if (itY != y.mDigits.rend())
+            {
+                s += *itY;
+                ++itY;
+            }
+            *itZ = s % 10;
+            carry = (s > 9) ? 1 : 0;
+            ++itZ;
+        }
+        if (carry != 0)
+        {
+            *itZ = carry;
+        }
+        if (z.mDigits.size() > 1 && z.mDigits.front() == 0)
+        {
+            z.mDigits.erase(z.mDigits.begin());
+        }
+        return z;
+    }
 };
 
 inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
@@ -41,3 +80,18 @@ inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
 
     return out;
 }
+
+inline BigInt operator+(const BigInt &x, const BigInt &y)
+{
+    if (!x.mIsNegative && !y.mIsNegative)
+    {
+        return BigInt::addAbsVaalues(x, y);
+    }
+
+    if (x.mIsNegative && y.mIsNegative)
+    {
+    }
+
+    throw std::runtime_error("not implemented yet");
+}
+-
--- project01/main.cpp
+++ project01/main.cpp
@@ -24,25 +24,69 @@ TEST_CASE("Default constructor")
     }
 }
 
-TEST_CASE("Constructor with  a string")
+// TEST_CASE("Constructor with  a string")
+// {
+//     ostringstream sout;
+//     SUBCASE("postive number")
+//     {
+//         BigInt x("123456789123456789");
+//         sout << x;
+//         REQUIRE(sout.str() == "123456789123456789");
+//     }
+
+//     SUBCASE("negative number")
+//     {
+//         BigInt x("-123");
+//         sout << x;
+//         REQUIRE(sout.str() == "-123");
+//     }
+
+//     SUBCASE("empty string")
+//     {
+//         REQUIRE_THROWS_AS(BigInt(""), runtime_error);
+//     }
+// }
+
+TEST_CASE("Addition")
 {
     ostringstream sout;
-    SUBCASE("postive number")
-    {
-        BigInt x("123456789123456789");
-        sout << x;
-        REQUIRE(sout.str() == "123456789123456789");
-    }
 
-    SUBCASE("negative number")
+    SUBCASE("positive + positive, test#1")
     {
-        BigInt x("-123");
-        sout << x;
-        REQUIRE(sout.str() == "-123");
+        BigInt x("193");
+        BigInt y("59");
+        sout << x + y;
+        REQUIRE(sout.str() == "252");
     }
-
-    SUBCASE("empty string")
+    SUBCASE("positive + positive, test#2")
     {
-        REQUIRE_THROWS_AS(BigInt(""), runtime_error);
+        for (int x = 0; x <= 1000; x++)
+        {
+            for (int y = 0; y <= 1000; y++)
+            {
+                BigInt a(to_string(x));
+                BigInt b(to_string(y));
+                sout << a + b;
+                REQUIRE(sout.str() == to_string(x + y));
+                sout.str("");
+            }
+        }
     }
+    // SUBCASE("positive + negative")
+    // {
+    //     BigInt x("999");
+    //     BigInt y("-5");
+    //     sout << x + y;
+    //     REQUIRE(sout.str() == "1000");
+    // }
+    SUBCASE("negative + positive")
+    {
+    }
+    SUBCASE("negative + negative")
+    {
+        BigInt x("-2");
+        BigInt y("-5");
+        sout << x + y;
+        REQUIRE(sout.str() == "-7");
+        }
 }
