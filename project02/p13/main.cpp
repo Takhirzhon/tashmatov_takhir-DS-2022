247db5ea4f37caa8c5991a511fd0eaf99c143b2e
Takhirzhon <tashmatov_t@auca.kg>
2022-12-20 10:27:15 +0100

    Finshed Problem 13

--- project02/p13/main.cpp
+++ project02/p13/main.cpp
@@ -1,32 +1,44 @@
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-// template <typename C>
-// int sz(const C &c) { return static_cast<int>(c.size()); }
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
 
-// using namespace std;
+using namespace std;
 
-// int main()
-// {
-//     iostream::sync_with_stdio(false);
+int main()
+{
+    iostream::sync_with_stdio(false);
+    string word;
+    int length = 0;
+    vector<pair<string, string>> dyslection;
+    while (getline(cin, word))
+    {
+        if (word.empty())
+        {
+            sort(dyslection.begin(), dyslection.end(),
+                 [](const pair<string, string> &a, const pair<string, string> &b)
+                 { return a.first < b.first; });
+            for (auto x : dyslection)
+            {
+                cout << setw(length) << x.second << "\n";
+            }
+            dyslection.clear();
+            length = 0;
+            cout << "\n";
+        }
 
-//     vector<string> word();
-//     char line;
-//     int length = 0;
-//     while (cin.get(line))
-//     {
-
-//         for (int i = 0; i++;)
-//         {
-//             cin >> word[i];
-//         }
-//         for (auto c : word)
-//         {
-//             reverse(c.begin(), c.end());
-//         }
-//         sort(word.begin(), word.end());
-//     }
-//     for (auto i : word)
-//     {
-//         cout << i << endl;
-//     }
-// }
+        else
+        {
+            string s = word;
+            reverse(s.begin(), s.end());
+            length = max(length, int(s.size()));
+            dyslection.push_back({s, word});
+        }
+    }
+    sort(dyslection.begin(), dyslection.end());
+    for (auto x : dyslection)
+    {
+        cout << setw(length) << x.second << "\n";
+    }
+    dyslection.clear();
+}
